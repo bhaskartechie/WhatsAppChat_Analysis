5f0d95410f00fec5ffc3122f76d027b2146da823
Bhaskar <Bhaskar.techiepro@gmail.com>
2020-11-11 16:55:37 +0530

    static_files setting changed

--- WhatsAppChat_Analysis/settings.py
+++ WhatsAppChat_Analysis/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'GroupChat', 'static/')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT= os.path.join(BASE_DIR,'GroupChat', 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
