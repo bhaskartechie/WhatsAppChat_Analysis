cd82bd1119bcf2c22e0d9cbb5fe91a5b35bbc12f
Bhaskar <bhaskar.techiepro@gmail.com>
2024-07-02 07:40:55 +0530

    Update settings.py

--- WhatsAppChat_Analysis/settings.py
+++ WhatsAppChat_Analysis/settings.py
@@ -77,23 +77,13 @@ WSGI_APPLICATION = 'WhatsAppChat_Analysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'WhatsAppChat',
-        'USER': 'postgres',
-        'PASSWORD': '1egBre@k',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
